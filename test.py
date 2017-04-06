d00088675976a777a82704c0b7dcaa38bfa49ded
root <root@tsds-frontend-el7-test.grnoc.iu.edu>
2017-04-06 19:20:40 +0000

    Query method

--- test.py
+++ test.py
@@ -5,29 +5,24 @@ import cgi
 import cgitb
 import sys
 import json
-import urllib2
+from urllib import urlencode
 import os
 from urllib2 import Request, urlopen, URLError, HTTPBasicAuthHandler, HTTPPasswordMgr, HTTPPasswordMgrWithDefaultRealm, build_opener, install_opener
 
 
-def search():
-	fileOut = open("/home/mthatte/verify.txt","rw+")
-	fileOut.write("Hi I am in search  query\n");
-	inpParameter = sys.stdin.read() # reading the input data sent in POST method
-	fileOut.write(inpParameter+"\n")
-	url = "https://tsds-services-el7-test.grnoc.iu.edu/tsds-basic/services/metadata.cgi?method=get_measurement_type_values;measurement_type=interface"
-	#Basic - Authetication
-		
+def auth_Connection(url):
 	username = "test_user"
-	password = "testpass123"
-	passman = HTTPPasswordMgrWithDefaultRealm() # creating a password manager
-	passman.add_password(None, url, username, password)
-	authhandler = HTTPBasicAuthHandler(passman)
-	opener = build_opener(authhandler)
-	install_opener(opener)	
-	
-	#Basic AUth done, whenevr urlopen is called, bsic-auth will get executed	
+        password = "testpass123"
+        passman = HTTPPasswordMgrWithDefaultRealm() # creating a password manager
+        passman.add_password(None, url, username, password)
+        authhandler = HTTPBasicAuthHandler(passman)
+        opener = build_opener(authhandler)
+        install_opener(opener)
 
+def search():
+	inpParameter = sys.stdin.read() # reading the input data sent in POST method
+	url = "https://tsds-services-el7-test.grnoc.iu.edu/tsds-basic/services/metadata.cgi?method=get_measurement_type_values;measurement_type=interface"
+	auth_Connection(url)
 	try:
 		request = Request(url)
 		response = urlopen(request)
@@ -39,13 +34,12 @@ def search():
 				if key =="name":
 					output.append(value)
 		
-		#fileOut.write(json_result + " \n")
         	print "Content-Type: application/json" # set the HTTP response header to json data
 		print "Cache-Control: no-cache\n"
 	        print json.dumps(output) #HTTP response 
 		
 	except URLError, e:
-		#print "Content-type: text/plain'\n"
+		print "Content-type: text/plain"
     		print 'Error opening tsds URL \n', e
 		if hasattr(e,'code'):
 			if e.code==401:
@@ -54,26 +48,38 @@ def search():
 			else:
 				print "different error code \n"
 	
-	'''
-
-	res = ["upper_25","upper_50","upper_75","upper_90","upper_95"] # dumy sample response text
-	json_result = json.dumps(res) #convert the response data into json formated string
-	#fileOut.write(json_result + " \n")
-	print "Content-type: application/json\n" # set the HTTP response header to json data
-	print json_result #HTTP response 
-	fileOut.close()
-	'''
-	
 def query():
-	fileOut = open("/home/mthatte/verify.txt","rw+")
-        fileOut.write("Hi I am in Query  query\n");
-        inpParameter = sys.stdin.read()
-        fileOut.write(inpParameter+"\n")
-        fileOut.close()
+	print "Content-Type: application/json\n"
+        inpParameter = json.loads(sys.stdin.read())
+	#get to the target field of json : 
+	tsds_query=""
+	for key,value in inpParameter.iteritems():
+		if key == "targets":
+			for eachElement in value:
+				tsds_query = eachElement["target"]
+	#print tsds_query
+	#print json.dumps(tsds_query)
 	
+	#Request data from tsds - 
+	url= "https://tsds-services-el7-test.grnoc.iu.edu/tsds-basic/services/query.cgi"
+	auth_Connection(url)
+	postParameters = {"method":"query","query":tsds_query}
+	try:
+		request = Request(url,urlencode(postParameters))
+		response = urlopen(request)
+	except URLError, e:
+       		print "Content-type: text/plain"
+         	print 'Error opening tsds server URL \n', e
+		if hasattr(e,'code'):
+                        if e.code==401:
+				print "Auth required"
+	#print response.read()
 
+	#Format the data received from tsds to grafana compatibale data - 
+
+		
 if __name__ == "__main__":
-	#print "Cache-Control: no-cache\n"
+	print "Cache-Control: no-cache"
 	inpUrl = os.environ["PATH_INFO"]
 	filemain = open("/home/mthatte/mainData.txt","rw+")
 	if inpUrl == "/":
